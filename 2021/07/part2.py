6b6ffa23b1a0d45c568da649e1ccb7351b4bd4bd
Panos Katseas <jiguromusha7@gmail.com>
2021-12-18 13:25:07 +0200

    Add day 07 part2 solution

---
+++ 2021/07/part2.py
@@ -0,0 +1,26 @@
+from os import path
+
+
+def main() -> None:
+    base_dir = path.dirname(path.abspath(__file__))
+    with open(path.join(base_dir, "input")) as crabs_input:
+        crabs = [int(crab) for crab in crabs_input.read().split(",")]
+
+    fuel = {}
+    for position in range(min(crabs), max(crabs) + 1):
+        fuel[position] = sum(sum(range(1, abs(position - crab) + 1)) for crab in crabs)
+
+    min_fuel = None
+    min_fuel_position = None
+    for position, fuel in fuel.items():
+        if min_fuel is None or fuel < min_fuel:
+            min_fuel = fuel
+            min_fuel_position = position
+
+    print(
+        f"The position requiring the least fuel is {min_fuel_position} with fuel {min_fuel}"
+    )
+
+
+if __name__ == "__main__":
+    main()
